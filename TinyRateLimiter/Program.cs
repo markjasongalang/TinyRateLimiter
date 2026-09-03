974812422d164dd1420c108b3dd3eb678d5616a5
markjasongalang <markjasongalang3@gmail.com>
2026-09-03 10:44:48 +0800

    feat: initial rate limiter

--- TinyRateLimiter/Program.cs
+++ TinyRateLimiter/Program.cs
@@ -1,8 +1,18 @@
 ﻿
+using TinyRateLimiter;
+
 public class Program
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var limiter = new RateLimiter(5);
+
+        for (var i = 1; i <= 10; i++)
+        {
+            bool allowed = limiter.AllowRequest();
+
+            Console.WriteLine(
+                $"Request {i}: {(allowed ? "Allowed" : "Rejected")}");
+        }
     }
 }
---
+++ TinyRateLimiter/RateLimiter.cs
@@ -0,0 +1,24 @@
+﻿namespace TinyRateLimiter;
+
+public class RateLimiter
+{
+    private readonly int _limit;
+    private int _requestCount;
+
+    public RateLimiter(int limit)
+    {
+        _limit = limit;
+    }
+
+    public bool AllowRequest()
+    {
+        if (_requestCount >= _limit)
+        {
+            return false;
+        }
+
+        _requestCount++;
+
+        return true;
+    }
+}
